4e37d8fb7e512e2a381fe9f8729b1738a7c2e992
Dale <drcorns@hotmail.com>
2016-01-28 11:45:51 -0800

    remove data entry that was add by mistake

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/', function (req, res, next)
   res.end(data.toString().replace(/host:port/g, req.header('Host')));
 });
 
-app.get(/\/(css|js|img|data|icon|small-slides)\/?.*/, restify.serveStatic({directory: './static/'}));
+app.get(/\/(css|js|img|icon|small-slides)\/?.*/, restify.serveStatic({directory: './static/'}));
 
 app.listen(config.get('PORT'), config.get('IP'), function () {
   console.log( "Listening on " + config.get('IP') + ", port " + config.get('PORT') )

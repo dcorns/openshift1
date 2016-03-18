bd68a9514b77d8a10185e066fbb9be4fe29d54aa
Dale <drcorns@hotmail.com>
2016-03-18 16:52:57 -0700

    start some refactoring

---
+++ app/js/helperMethods.js
@@ -0,0 +1,13 @@
+/**
+ * helperMethods.js
+ * Created by dcorns on 3/18/16
+ * Copyright © 2016 Dale Corns
+ */
+'use strict';
+module.exports = function(){
+  return{
+    toggleAccessOptions: function toggleAccessOptions(access){
+    
+  }
+  };
+};

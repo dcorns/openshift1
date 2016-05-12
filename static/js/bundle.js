8474f8a5e7ae8097bfda0ca6613bacfa54263fbd
Dale <drcorns@hotmail.com>
2016-05-12 12:36:15 -0700

    setup a test structure

--- static/js/bundle.js
+++ static/js/bundle.js
@@ -850,6 +850,8 @@
 	    if(err){
 	      alert('No profile data found locally. Internet required to load profile data. Or you are not authorized for a profile account');
 	      window.location = '#/current';
+	      console.dir(err);
+	      console.dir(data);
 	    }
 	    else{
 	      myData = data;

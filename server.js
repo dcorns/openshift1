86c5508c1dfce055c3d285562074dcd4955af5d0
Dale <drcorns@hotmail.com>
2016-05-27 23:50:03 -0700

    remove dependence on config-multipass

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ var cc          = require('config-multipaas'),
 var config      = cc(),
     app         = restify.createServer();
 
+var port = process.env.PORT || 8000;
+var ip = process.env.BIND_IP || 'localhost';
+console.log(port);
 corngoose.startDB('drc');
 
 app.use(restify.queryParser());
@@ -16,7 +19,7 @@ app.use(restify.bodyParser());
 
 // Routes
 require('./api/routes/routes')(app);
-console.log(process.argv);
+
 let webRoot = process.argv[2] || './static/';
 app.get(/\/(css|js|img|icon|small-slides)\/?.*/, restify.serveStatic({directory: webRoot}));
 
@@ -30,6 +33,9 @@ app.get('/', function (req, res, next)
   res.end(data.toString().replace(/host:port/g, req.header('Host')));
 });
 
-app.listen(config.get('PORT'), config.get('IP'), function () {
-  console.log( "Listening on " + config.get('IP') + ", port " + config.get('PORT') );
+// app.listen(config.get('PORT'), config.get('IP'), function () {
+//   console.log( "Listening on " + config.get('IP') + ", port " + config.get('PORT') );
+// });
+app.listen(port, ip, function () {
+  console.log( "Listening on ", ip, ", port ", port );
 });
